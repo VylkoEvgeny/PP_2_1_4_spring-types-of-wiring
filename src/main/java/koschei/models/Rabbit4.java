05d52b9f218a2acbe72f2c3a3c1f375382ba7421
Evgeny Vylko <belfoot10@gmail.com>
2026-05-21 10:22:07 +0300

    make duck final

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
-    private Duck5 duck;
+    private final Duck5 duck;
 
     public Rabbit4(Duck5 duck) {
         this.duck = duck;
